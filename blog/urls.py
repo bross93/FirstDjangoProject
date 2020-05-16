02a3ae01ce1a5ffb19baccf1b1dea97e31d571a1
Robert <bobby.ross93@gmail.com>
2020-05-16 17:11:33 -0600

    Added view and template for post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('posts/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+
+
+##### NOTES: #####
+# - Perhaps change the <int:> with a string later down the road to show the practice name
