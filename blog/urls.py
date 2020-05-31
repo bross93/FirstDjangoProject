46059dacb91289b03b694e58f5af87eaf60b8e95
Robert <bobby.ross93@gmail.com>
2020-05-31 14:36:10 -0600

    added the practices skeleton

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+	path('', views.home_view, name='home_view'),
+    path('listing', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,9 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
 # Create your views here.
+def home_view(request):
+	return render(request, 'blog/home.html')
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
